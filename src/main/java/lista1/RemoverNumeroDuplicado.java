78b070b85bad992cb9a5acff7220a9f58de1a4ab
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-12 14:02:07 -0300

    Criando metodos remover e encontar numeros duplicados

--- src/main/java/lista1/RemoverNumeroDuplicado.java
+++ src/main/java/lista1/RemoverNumeroDuplicado.java
@@ -6,12 +6,27 @@ public class RemoverNumeroDuplicado {
     public static void main(String[] args){
         List<Integer> numeros = Arrays.asList(3, 4, 5, 3, 2, 1, 2);
         List<Integer> resultado = removerDuplicados(numeros);
-        System.out.println(resultado);
+
+        List<Integer> numerosRetirados = encontrarDuplicados(numeros);
+
+        System.out.println("Números retirados: " + numerosRetirados);
+        System.out.println("Números sem duplicação: " + resultado);
 
     }
     public static List<Integer> removerDuplicados(List<Integer> numeros) {
         Set<Integer> set = new LinkedHashSet<>(numeros);
         return new ArrayList<>(set);
+    }
+    public static List<Integer> encontrarDuplicados(List<Integer> numeros) {
+        Set<Integer> numerosUnicos = new HashSet<>();
+        List<Integer> duplicados = new ArrayList<>();
+
+        for (Integer numero : numeros) {
+            if (!numerosUnicos.add(numero)) {
+                duplicados.add(numero);
+            }
+        }
+        return duplicados;
     }
 
 
--- src/test/java/lista1/TestRemoverNumeroDuplicado.java
+++ src/test/java/lista1/TestRemoverNumeroDuplicado.java
@@ -1,4 +1,26 @@
 package lista1;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestRemoverNumeroDuplicado {
+    @Test
+    public void testRemoveNumeroDuplicados() {
+        List<Integer> numeros = Arrays.asList(3, 4, 5, 3, 2, 1, 2);
+        List<Integer> resultadoEsperado = Arrays.asList(3, 4, 5, 2, 1);
+        List<Integer> resultado = RemoverNumeroDuplicado.removerDuplicados(numeros);
+
+        numeros.removeAll(resultado);
+        System.out.println("Números retirados: " + numeros);
+
+        System.out.println("Números sem duplicação: " + resultado);
+
+        assertEquals(resultadoEsperado, resultado);
+
+
+    }
+
 }
