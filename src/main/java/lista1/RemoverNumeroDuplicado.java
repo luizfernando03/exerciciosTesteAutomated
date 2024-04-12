33accbfb42be9f5410260b182aae94a0c679c5ea
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-12 11:34:23 -0300

    Subindo dados da classe removerNumeroDuplicado

--- src/main/java/lista1/RemoverNumeroDuplicado.java
+++ src/main/java/lista1/RemoverNumeroDuplicado.java
@@ -1,7 +1,18 @@
 package lista1;
 
+import java.util.*;
+
 public class RemoverNumeroDuplicado {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        List<Integer> numeros = Arrays.asList(3, 4, 5, 3, 2, 1, 2);
+        List<Integer> resultado = removerDuplicados(numeros);
+        System.out.println(resultado);
 
     }
+    public static List<Integer> removerDuplicados(List<Integer> numeros) {
+        Set<Integer> set = new LinkedHashSet<>(numeros);
+        return new ArrayList<>(set);
+    }
+
+
 }
